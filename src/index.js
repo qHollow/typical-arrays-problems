851c1b6b8ec447a127d067d854772202e391115a
Reshetnikov Dmitriy <nighthunter2015@gmail.com>
2020-10-03 22:18:57 +0300

    init: start task

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+exports.min = function min (array) {
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else{
+    return Math.min.apply(Math, array);
+  }
+}
+
+exports.max = function max (array) {
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else{
+  return Math.max.apply(Math, array);
+  }
+}
+
+exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else{
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
+}
